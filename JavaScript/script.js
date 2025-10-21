349a8ef4528c73f642a234bc8514ac4041c1b531
TalhaDevDaily <talha@boishakhikunja.onmicrosoft.com>
2025-10-22 01:15:10 +0600

    Destructuring, spread, rest

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,5 +1,5 @@
-const alphabet = ["A", "B", "C", "D", "E", "F"];
-const numbers = ["1", "2", "3", "4", "5", "6"];
+// const alphabet = ["A", "B", "C", "D", "E", "F"];
+// const numbers = ["1", "2", "3", "4", "5", "6"];
 
 // Destructuring in Arrays
 /*
@@ -24,6 +24,7 @@ const [sum, multiply, division = "No division"] = sumAndMultiply(2, 3);
 */
 
 // Destructuring in Objects
+/*
 const personOne = {
   name: "Kyle",
   age: 24,
@@ -53,3 +54,49 @@ function printUser({ name, age, favouriteFood = " Watermelon" }) {
 }
 
 printUser(personOne);
+*/
+
+// Spread Operator
+/*
+let numbers = [1, 2, 3, 4, 5];
+
+let maximum = Math.max(...numbers);
+let minimum = Math.min(...numbers);
+
+// console.log(maximum);
+// console.log(minimum);
+
+let username = "Abu Talha Tamim";
+let letters = [...username].join("-");
+
+console.log(letters);
+
+let fruits = ["apple", "orange", "banana"];
+let vegetables = ["carrots", "celery", "potato"];
+
+let foods = [...fruits, ...vegetables, "eggs", "milk"];
+
+console.log(foods);
+*/
+
+// Rest Parameters
+/*
+function openFridge(...foods) {
+  console.log(...foods);
+}
+
+function getFood(...foods) {
+  return foods;
+}
+
+const food1 = "pizza";
+const food2 = "hamburger";
+const food3 = "hotdog";
+const food4 = "sushi";
+const food5 = "ramen";
+
+// openFridge(food1, food2, food3, food4, food5);
+
+const foods = getFood(food1, food2, food3, food4, food5);
+// console.log(foods);
+*/
